f3d6d1979db1ad355c94d94b6d8690d8ff8b25f0
Paloma <psecunda@elon.edu>
2019-10-04 09:23:14 -0400

    Check out new branch

--- frontend/actions/project_actions.js
+++ frontend/actions/project_actions.js
@@ -1,1 +1,5 @@
 import * as APIUtil from '../util/project_api_utils'; 
+
+export const RECEIVE_PROJECT = "RECEIVE_PROJECT"; 
+export const RECEIVE_PROJECTS = "RECEIVE_PROJECTS"; 
+export const REMOVE_PROJECT = "REMOVE_PROJECT"; 
