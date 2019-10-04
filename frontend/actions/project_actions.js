1df660e9d5741def3ff5d67660ddcfee97b41a31
Paloma <psecunda@elon.edu>
2019-10-04 10:03:01 -0400

    Finish project actions

--- frontend/actions/project_actions.js
+++ frontend/actions/project_actions.js
@@ -1,5 +1,41 @@
-import * as APIUtil from '../util/project_api_utils'; 
+import { fetchProject, fetchProjects, destroyProject, editProject, createProject } from '../util/project_api_utils'; 
+import { receiveCurrentUser } from './session_actions'; 
 
 export const RECEIVE_PROJECT = "RECEIVE_PROJECT"; 
 export const RECEIVE_PROJECTS = "RECEIVE_PROJECTS"; 
-export const REMOVE_PROJECT = "REMOVE_PROJECT"; 
+
+export const receiveProject = project => {
+    return {
+        type: RECEIVE_PROJECT, 
+        project
+    }
+}
+
+export const receiveProjects = projects => {
+    return {
+        type: RECEIVE_PROJECTS, 
+        projects
+    }
+}
+
+export const 
+
+export const makeProject = project => dispatch => {
+    return createProject(project).then(project => dispatch(receiveProject(project)))
+}
+
+export const showProject = id => dispatch => {
+    return fetchProject(id).then(project => dispatch(receiveProject(project)))
+}
+
+export const allProjects = () => dispatch => {
+    return fetchProjects().then(projects => dispatch(receiveProjects(projects)))
+}
+
+export const deleteProject = id => dispatch => {
+    return destroyProject(id).then(user => dispatch(receiveCurrentUser(user)))
+}
+
+export const updateProject = project => dispatch => {
+    return editProject(project).then(project => dispatch(receiveProject(project)))
+}
