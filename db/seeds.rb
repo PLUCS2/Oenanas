e21dbd8816ecee2bf0fd19b864e82a7c598414ad
Paloma <psecunda@elon.edu>
2019-10-08 14:03:05 -0400

    Seed projects in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Project.destroy_all
 User.destroy_all
 
-demo_user = User.create( {first_name: "Demo", last_name: "Demo", email: "demo@demo.com", password: "12345678", role: "Demo Login"})
+demo_user = User.create({first_name: "Demo", last_name: "Demo", email: "demo@demo.com", password: "12345678", role: "Demo Login"})
+
+project_board = Project.create({title: "Demo Project", owner_id: demo_user.id, description: "Demo Board View", default_view: "board"})
+project_list = Project.create({title: "Demo Project", owner_id: demo_user.id, description: "Demo List View", default_view: "list"})
