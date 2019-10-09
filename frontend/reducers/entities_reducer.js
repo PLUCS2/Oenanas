8f5f967e99c93f639154a798237cc0e11da84d4d
Paloma <psecunda@elon.edu>
2019-10-09 10:36:00 -0400

    Complete entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'; 
 import usersReducer from './users_reducer'; 
 import projectsReducer from './projects_reducer'; 
+import sectionsReducer from './sections_reducer'; 
+import tasksReducer from './tasks_reducer'; 
 
 export default combineReducers({
     users: usersReducer, 
-    projects: projectsReducer
+    projects: projectsReducer, 
+    sections: sectionsReducer, 
+    tasks: tasksReducer
 })
