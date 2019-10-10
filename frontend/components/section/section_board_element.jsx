e9fc2b3cec75fdfaf5d73c4aa799413d154866d6
Paloma <psecunda@elon.edu>
2019-10-10 12:14:31 -0400

    Part of section delete button

--- frontend/components/section/section_board_element.jsx
+++ frontend/components/section/section_board_element.jsx
@@ -8,6 +8,8 @@ class SectionBoardElement extends React.Component {
         this.state = this.props.section; 
         this.changeSectionName = this.changeSectionName.bind(this); 
         this.updateSecName = this.updateSecName.bind(this); 
+        this.showDelete = this.showDelete.bind(this); 
+        this.closeDelete = this.closeDelete.bind(this); 
         // debugger; 
     }
 
@@ -21,14 +23,36 @@ class SectionBoardElement extends React.Component {
         this.props.otherProps.editSection({section: this.state})
     }
 
+    showDelete(e) {
+        e.preventDefault(); 
+        document.getElementById("drop-delete-button").className = "show-drop-delete-button";
+        document.addEventListener("click", this.closeDelete); 
+    }
+
+    closeDelete(e) {
+        e.preventDefault(); 
+        if (!e.target.matches(".section-delete-dropdown") && e.target.nodeName != "A") {
+            document.getElementById("drop-delete-button").className = "drop-delete-button";
+            document.removeEventListener("click", this.closeDelete);
+        }
+    }
+
     render() {
         return(
-            <input type="text" 
-            key={this.props.section.id} 
-            className="column-heading-boardview" 
-            value={this.state.name} 
-            onBlur={this.updateSecName}
-            onChange={this.changeSectionName} />
+            <>
+                <input type="text" 
+                key={this.props.section.id} 
+                className="column-heading-boardview" 
+                value={this.state.name} 
+                onBlur={this.updateSecName}
+                onChange={this.changeSectionName} />
+            <div className="section-delete-dropdown">
+                <button className="delete-section" onClick={this.showDelete}>...</button>
+                    <div id="drop-delete-button" className="drop-delete-button">
+                        <button>Delete Section</button>
+                    </div>
+            </div>
+            </>
         )
     }
 
